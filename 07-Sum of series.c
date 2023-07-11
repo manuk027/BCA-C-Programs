c2bc0b72631b119890ea6d2b9c265372cbf045b2
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-11 19:30:38 +0530

    Update and rename 7-.c to 07-Sum of series.c

---
+++ 07-Sum of series.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+double series (int n)
+{
+    int i;
+    double sum=0.0,ser;
+    for(i=1;i<=n;i++)
+    {
+        ser=1/pow(i,i);
+        sum+=ser;
+    }
+    return sum;
+}
+void main()
+{
+    int n;
+  clrscr();
+    printf("enter the limit");
+    scanf("%d",&n);
+    printf("\nsum of the series is:%f",series(n));
+    getch();
+}

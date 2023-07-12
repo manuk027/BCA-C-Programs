8bd7f6a0bf64aeae13a46f07b198638a723e7de9
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-12 19:34:06 +0530

    Update and rename 8-.c to 8-Number pattern.c

---
+++ 8-Number pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+    int i,n;
+    clrscr();
+    printf("Number pattern");
+    printf("\nEnter a number of rows:");
+    scanf("%d",&n);
+    for(i=10; i<n; i*=10);
+    for(i=i/10; n>0; i/=10)
+    {
+        printf("%d\n", n);
+        n%=i;
+    }
+    getch();
+}

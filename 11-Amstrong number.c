30b3276996b62fb319e10d687f54ac344fb239a2
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-13 19:03:48 +0530

    Create 11-Amstrong number.c

---
+++ 11-Amstrong number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i,t,c,d,s=0;
+    clrscr();
+    printf("\nEnter the range from 0 to: ");
+    scanf("%d",&n);
+    printf("\nArmstrong Numbers are: ");
+    for(i=0;i<=n;i++)
+    {
+        t=i;
+        s=0;
+        while(t!=0)
+        {
+            d=t%10;
+            c=d*d*d;
+            s=s+c;
+            t=t/10;
+        }
+        if(s==i)
+        printf("\n%d",i);
+    }
+    getch();
+}

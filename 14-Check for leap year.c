0656ee9b190f96fca2abba1a827a809c19a08a81
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-15 18:56:12 +0530

    Create 14-Check for leap year.c

---
+++ 14-Check for leap year.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+  int year;
+  clrscr();
+  printf("Enter an year");
+  scanf("%d",&year);
+  if(year%400==0)
+  {
+    printf("%d is a leap year\n",year);
+  }
+  else if (year%100==0)
+  {
+    printf("%d is not a leap year\n",year);
+  }
+  else if(year%4==0)
+  {
+    printf("%d is a leap year\n",year);
+  }
+  else
+  {
+    printf("%d is not a leap year\n",year);
+  }
+  getch();
+}

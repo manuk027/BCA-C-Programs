e1832c4763ae05aa7c72b644279df202ef2b0c66
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-15 20:47:14 +0530

    Update 02-Fibonacci Series.c

--- 02-Fibonacci Series.c
+++ 02-Fibonacci Series.c
@@ -1,19 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    
-int n;
+    int n;
 int i;
 int f3;
 int f1=0;
 int f2=1;
-
 printf("Enter the number:");
 scanf("%d",&n);
-
 printf("The fibonnaci series:\n");
 printf("%d\n%d\n",f1,f2);
-
 for (i=3;i<=n;i++)
     {
     f3=f1+f2;
@@ -21,6 +17,5 @@ for (i=3;i<=n;i++)
     f1=f2;
     f2=f3;
     }
-
 return 0;
 }

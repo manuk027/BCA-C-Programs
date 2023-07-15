78739601467ddbf821f9ed709aa0252fdb0fc902
Manu Krishna <107403309+manukrishna27112004@users.noreply.github.com>
2023-07-15 20:54:35 +0530

    Update 01-Sum and reverse.c

--- 01-Sum and reverse.c
+++ 01-Sum and reverse.c
@@ -2,21 +2,21 @@
 #include<conio.h>
 void main()
 {
-	int num;
-	int d;
-	int sum=0;
-	int rev=0;
-	clrscr();
-	printf("Enter a number:");
-	scanf("%d",&num);
-	while(num > 0)
-	{
-		d=num % 10;
-		sum=sum+d;
-		num=num/10;
-		rev=(rev*10)+d;	
-	}
-	printf("\nSum of the digits of the given number:%d \n", sum);
-	printf("Reverse of the given number is: %d" ,rev);
-	getch();
+int num;
+int d;
+int sum=0;
+int rev=0;
+clrscr();
+printf("Enter a number:");
+scanf("%d",&num);
+while(num > 0)
+{
+d=num % 10;
+sum=sum+d;
+num=num/10;
+rev=(rev*10)+d;	
+}
+printf("\nSum of the digits of the given number:%d \n", sum);
+printf("Reverse of the given number is: %d" ,rev);
+getch();
 }

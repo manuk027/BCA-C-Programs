f0e276ba582f1245d3c230f8d241bc11dcca5f9e
Manu Krishna <krishnamanu27112004@gmail.com>
2023-07-16 12:01:37 +0530

    Create 16-Base convertion.c

---
+++ 16-Base convertion.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    unsigned dec, t, d, i;
+    char bin[20], oct[15], hec[5];
+    clrscr();
+    printf("Enter a decimal number:");
+    scanf("%d", &dec);
+    t=dec;
+    i=0;
+    do
+    {
+        d=t%2;
+        t=t/2;
+        bin[i++]=d+48;
+    }
+    while(t!=0);
+    {
+        bin[i]='\0';
+        strrev(bin);
+        t=dec;
+        i=0;
+    }
+    do
+    {
+        d=t%8;
+        t=t/8;
+        oct[i++]=d+48;
+    }
+    while(t!=0);
+    {
+        oct[i]='\0';
+        strrev(oct);
+        t=dec;
+        i=0;
+    }
+    do
+    {
+        d=t%16;
+        t=t/16;
+        if(d>9)
+        {
+            hex[i++]=d+55;
+        }
+        else
+        {
+            hex[i++]=d+48;
+        }
+    }
+    while(t!=0);
+    {
+        hex[i]='\0';
+        strrev(hex);
+    }
+    printf("\nBinary equivalent of %u iS: %s", dec, bin);
+    printf("\nOctal equivalent of %u iS: %s", dec, oct);
+    printf("\nHexadecimal equivalent of %u iS: %s", dec, hex);
+    getch();
+}

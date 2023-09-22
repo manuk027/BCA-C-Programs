68d163552574883b29acf155d09dd4e0b28c9975
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-22 18:55:07 +0530

    Update 03-Pyramid pattern.c

--- 03-Pyramid pattern.c
+++ 03-Pyramid pattern.c
@@ -2,24 +2,18 @@
 #include<conio.h>
 void main()
 {
-    int r;
-    int s;
-    int t=0;
-    int rows=0;
+    int r, s, t=0, rows=0;
     clrscr();
-    
     printf("Enter the number of rows to print the pyramid:");
-    scanf("%d",&rows);
-    
-    printf("\nThe pyramid with %d rows are printed\n\n\n\n",rows);
-    
-    for (r=1; r<=rows; ++r, t=0)
+    scanf("%d", &rows);
+    printf("\nThe pyramid with %d rows are printed\n\n\n\n", rows);
+    for(r=1; r<=rows; ++r, t=0)
     {
-        for (s=1; s<=rows-r; ++s)
+        for(s=1; s<=rows-r; ++s)
         {
             printf(" ");
         }
-        while (t!=2*r-1)
+        while(t!=2*r-1)
         {
             printf("*");
             ++t;

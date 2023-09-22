259165ee2911964132b63746f22fe229d7aa7323
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-22 09:48:53 +0530

    Create Average of prime-21.c

---
+++ Average of prime-21.c
@@ -0,0 +1,46 @@
+#include<stdio.h>  
+#include<conio.h>
+  
+  void averageofprime(int prime[], int size) {  
+      int sum = 0;  
+      float avg;  
+      int i;  
+      for (i = 0; i < size; i++) {  
+          sum += prime[i];  
+      }  
+  
+      avg = (float)sum / size;  
+      printf("The average of prime numbers is %.2f\n", avg);  
+  }  
+  
+  int main() {  
+      int range, prime[100];  
+      int i, j=0, k, h=0, flag;
+clrscr();
+      printf("Enter a range: ");  
+      scanf("%d", &range);  
+          for (i = 0; i <= range; i++) {  
+          if (i == 1 || i == 0)  
+              continue;  
+  
+          flag = 1;  
+  
+          for (j = 2; j <= i / 2; ++j) {  
+              if (i % j == 0) {  
+                  flag = 0;  
+                  break;  
+              }  
+           }  
+  
+          if (flag == 1){  
+              printf("%d\n", i);  
+              prime[h]=i; 
+              h++; 
+           }  
+  
+  
+      }  
+  
+      averageofprime(prime, h);  
+      return 0;  
+  }

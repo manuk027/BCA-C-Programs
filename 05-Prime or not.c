10bb506e8fb2aaa15a25257f58b0eebad896a248
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-22 18:58:06 +0530

    Update 05-Prime or not.c

--- 05-Prime or not.c
+++ 05-Prime or not.c
@@ -1,23 +1,18 @@
 #include<stdio.h>
 #include<conio.h>
-
 void main()
 {
-  int i;
-  int r=0;
-  int number;
+  int i, number, r=0;
   clrscr();
-  
   printf("Enter any positive number:");
   scanf("%d",&number);
-  
   for(i=2; i<number-1; i++)
   {
     if (number%i==0)
     r=1;
     break;
   }
-  if (r==0)
+  if(r==0)
   printf("The number %d is prime", number);
   else
   printf("The number %d is not prime", number);

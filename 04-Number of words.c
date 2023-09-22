dbb8798bd606fc012c7fc18b1776ed3a4f60e0a7
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-22 18:56:30 +0530

    Update 04-Number of words.c

--- 04-Number of words.c
+++ 04-Number of words.c
@@ -4,18 +4,15 @@
 int main()
 {
 char s[200];
-int count=0;
-int i;
+int i, count=0;
 clrscr();
-
 printf("Enter the string:\n");
-scanf("%[^\n]s",s);
-
-for(i=0;s[i]!='\0';i++)
-    {
-        if (s[i]==' '&&s[i+1]!=' ')
-        count++;
-    }
-printf("Number of words in given String %d\n",count+1);
+scanf("%[^\n]s", s);
+for(i=0; s[i]!='\0'; i++)
+{
+    if (s[i]==' '&&s[i+1]!=' ')
+    count++;
+}
+printf("Number of words in given String %d\n", count+1);
 getch();
 }

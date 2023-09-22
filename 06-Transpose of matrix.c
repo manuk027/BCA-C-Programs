da650995533aee32a1d30c2e783120cbae8c1968
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-22 19:04:06 +0530

    Update 06-Transpose of matrix.c

--- 06-Transpose of matrix.c
+++ 06-Transpose of matrix.c
@@ -2,9 +2,7 @@
 #include<conio.h>
 void main()
 {
-    int arr1[50][50];
-    int brr1[50][50];
-    int i, j, r, c;
+    int i, j, r, c, arr1[50][50], brr1[50][50];
     clrscr();
     printf("\n\nTranspose of matrix:\n");
     printf("----------------------------\n");
@@ -13,39 +11,39 @@ void main()
     printf("\nInput the number of coloumns of the matrix:");
     scanf("%d", &c);
     printf("Input elementsin the first matrix :\n");
-    for(i=0;i<r;i++)
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
         {
-            for(j=0;j<c;j++)
-                {
-                    printf("Elements -[%d],[%d] : ",i,j);
-                    scanf("%d",&arr1[i][j]);
-                }
+            printf("Elements -[%d],[%d] : ", i, j);
+            scanf("%d", &arr1[i][j]);
         }
+    }
     printf("\nThe matrix is :\n");
-    for(i=0;i<r;i++)
-        {
-            printf("\n");
-            for(j=0;j<c;j++)
-                {
-                    printf("%d\t",arr1[i][j]);
-                }
-        }
-    for(i=0;i<r;i++)
-        {
-            for(j=0;j<c;j++)
-                {
-                    brr1[j][i]=arr1[i][j];
-                }
-        }
-    printf("\n\nThe transpose of the matrix is: ");
-    for(i=0;i<c;i++)
+    for(i=0; i<r; i++)
     {
         printf("\n");
-        for(j=0;j<r;j++)
-            {
-                printf("%d\t",brr1[i][j]);
-            }
+        for(j=0; j<c; j++)
+        {
+            printf("%d\t", arr1[i][j]);
+        }
     }
-printf("\n\n");
-getch();
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+            brr1[j][i]=arr1[i][j];
+        }
+    }
+    printf("\n\nThe transpose of the matrix is: ");
+    for(i=0; i<c; i++)
+    {
+        printf("\n");
+        for(j=0; j<r; j++)
+        {
+            printf("%d\t", brr1[i][j]);
+        }
+    }
+    printf("\n\n");
+    getch();
 }

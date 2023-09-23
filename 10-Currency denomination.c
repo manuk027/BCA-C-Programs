911a4dbcc6a91a651fdb448d0839b8506816fceb
Manu Krishna <krishnamanu27112004@gmail.com>
2023-09-23 07:07:15 +0530

    Update 10-Currency denomination.c

--- 10-Currency denomination.c
+++ 10-Currency denomination.c
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main()
 {
-  int a[8]={500,100,50,20,10,5,2,1},m,temp,i;
+  int a[8]={500, 100, 50, 20, 10, 5, 2, 1}, m, temp, i;
   clrscr();
-  printf(" Enterthe amount:");
-  scanf("%d",&m);
+  printf("Enterthe amount:");
+  scanf("%d", &m);
   temp=m;
-  for(i=0;i<8;i++)
+  for(i=0; i<8; i++)
   {
-    printf("\n%d notes:%d",a[i],temp/a [i]);
+    printf("\n%d notes:%d", a[i], temp/a[i]);
     temp=temp%a[i];
   }
 getch();
